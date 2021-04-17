b862d3f0132b17d97970bb9389c70e12de76fb80
nobuhiroharada <nobuhiroharada1026@gmail.com>
2021-04-17 13:10:49 +0900

    ダークモード切り替えのスタイル修正

--- src/components/header.js
+++ src/components/header.js
@@ -21,16 +21,18 @@ const Header = () => {
 						{data.site.siteMetadata.title}
 					</Link>
 				</h1>
-				<div onClick={() => setTheme(colorTheme)} className=''>
-					{colorTheme === 'light' ?
-						<svg xmlns="http://www.w3.org/2000/svg" className="text-gray-200 cursor-pointer w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-						</svg>
-						:
-						<svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-						</svg>
-					}
+				<div className="self-start">
+					<div onClick={() => setTheme(colorTheme)}>
+						{colorTheme === 'light' ?
+							<svg xmlns="http://www.w3.org/2000/svg" className="text-gray-200 cursor-pointer w-7" viewBox="0 0 20 20" fill="currentColor">
+								<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+							</svg>
+							:
+							<svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+							</svg>
+						}
+					</div>
 				</div>
 			</div>
 			<nav>
